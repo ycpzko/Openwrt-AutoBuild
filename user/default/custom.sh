b8a1f0c8353f1bc637e740864a2292c8f83c0e3d
hyird <31695447+hyird@users.noreply.github.com>
2020-04-07 07:23:14 +0800

    Update custom.sh

--- user/default/custom.sh
+++ user/default/custom.sh
@@ -5,5 +5,9 @@ echo "Test custom.sh"
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
+#更改后台登陆ip为：10.0.0.1
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+
+#修改openwrt_release版本
+sed 's#R[0-9].*\('\''\)#R20.4.7\1#g'
 
