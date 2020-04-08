faa18c78a240ac79b94f625bf6c8cd3b450e145b
hyird <31695447+hyird@users.noreply.github.com>
2020-04-08 18:10:42 +0800

    Update custom.sh

--- user/default/custom.sh
+++ user/default/custom.sh
@@ -9,7 +9,7 @@ echo "Test custom.sh"
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 #修改openwrt_release版本
-sed 's#R[0-9].*\('\''\)#R20.4.7\1#g'   package/lean/default-settings/files/zzz-default-settings
+sed -i 's#R[0-9].*\('\''\)#R20.4.7\1#g'   package/lean/default-settings/files/zzz-default-settings
 
 #去除密码
 sed  -i '/^sed.*shadow$/d'  package/lean/default-settings/files/zzz-default-settings
