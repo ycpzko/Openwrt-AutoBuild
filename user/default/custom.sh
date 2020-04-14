48f436ba421b42d799ace9ca7c94c105078002cf
ycpzko <50710126+ycpzko@users.noreply.github.com>
2020-04-14 20:53:39 +0800

    Update custom.sh

--- user/default/custom.sh
+++ user/default/custom.sh
@@ -5,18 +5,6 @@ echo "Test custom.sh"
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
-#更改后台登陆ip为：10.0.0.1
-sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+#更改后台登陆ip为：192.168.6.254
+sed -i 's/192.168.1.1/192.168.6.254/g' package/base-files/files/bin/config_generate
 
-#修改openwrt_release版本
-sed -i 's#R[0-9].*\('\''\)#R20.4.8\1#g'   package/lean/default-settings/files/zzz-default-settings
-sed -i 's/https:/R20.4.8/g' package/lean/default-settings/files/zzz-default-settings
-sed -i 's/http:/snapshots/g' package/lean/default-settings/files/zzz-default-settings
-sed -i 's/openwrt.proxy.ustclug.org/op.hyird.xyz/g' package/lean/default-settings/files/zzz-default-settings
-#去除密码
-sed  -i '/^sed.*shadow$/d'  package/lean/default-settings/files/zzz-default-settings
-
-#添加一些软件包
-git clone https://github.com/rufengsuixing/luci-app-adguardhome package/
-git clone https://github.com/destan19/OpenAppFilter package/
-git clone https://github.com/Mrbai98/luci-theme-atmaterial.git
